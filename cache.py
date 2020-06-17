1746f0674f5215fd4b8c165aff8f82e5564febd6
Lev Lamberov <dogsleg@debian.org>
2020-06-17 22:10:58 +0500

    Minor fixes

--- cache.py
+++ cache.py
@@ -2,11 +2,11 @@ import sqlite3
 
 
 class Cache():
-    def __init__(self):
-        self._conn = sqlite3.connect('cache.sqlite')
+    def __init__(self, db):
+        self._conn = sqlite3.connect(db)
         self._cur = self._conn.cursor()
 
-        self._cur.executescript('''CREATE TABLE IF NOT EXISTS ArchivesUpdates (
+        self._cur.executescript('''CREATE TABLE IF NOT EXISTS Updates (
                                    archive TEXT UNIQUE,
                                    last_update TEXT);
 
@@ -26,16 +26,16 @@ class Cache():
                                    layer TEXT)''')
 
         self._cur.execute('''SELECT name FROM sqlite_master
-                             WHERE type="table" AND name!="ArchivesUpdates"''')
+                             WHERE type="table" AND name!="Updates"''')
         self._tables = [table for (table,) in self._cur.fetchall()]
 
     def last_update(self, archive):
-        self._cur.execute('''SELECT last_update FROM ArchivesUpdates
+        self._cur.execute('''SELECT last_update FROM Updates
                              WHERE archive=?''', (archive,))
         return self._cur.fetchone()[0]
 
     def update_last_update(self, archive, last_update):
-        self._cur.execute('''INSERT OR REPLACE INTO ArchivesUpdates
+        self._cur.execute('''INSERT OR REPLACE INTO Updates
                              VALUES (?,?)''', (archive, last_update))
         self._conn.commit()
 
@@ -47,6 +47,8 @@ class Cache():
             layer = self._cur.fetchone()
             if layer:
                 layers[suit.lower()] = layer[0]
+            else:
+                layers[suit.lower()] = ''
         return layers
 
     def get_packages(self):
@@ -62,6 +64,15 @@ class Cache():
         return pkgs
 
     def _update_package(self, pkg):
+        self._cur.execute('''SELECT id FROM Packages
+                             WHERE pkg_name=?''', (pkg['pkg_name'],))
+        try:
+            id = self._cur.fetchone()[0]
+            for suit in ['Spacemacs', 'DoomEmacs']:
+                command = f'''DELETE FROM {suit} WHERE id=?'''
+                self._cur.execute(command, (id,))
+        except TypeError:
+            pass    # no such package, nothing to delete from suits
         self._cur.execute('''INSERT OR REPLACE INTO Packages
                              (pkg_name, homepage, in_debian, is_builtin)
                              VALUES (?, ?, ?, ?)''', (pkg['pkg_name'],
@@ -92,12 +103,33 @@ class Cache():
                 self._cur.execute(delete_command, (id,))
             self._conn.commit()
         except TypeError:
-            print(f'Package {pkg} in not in the cache database.')
+            print(f'Package {pkg} not found in cache.')
 
     def is_builtin(self, pkg):
-        self._cur.execute('''SELECT id FROM Packages
-                             WHERE Packages.pkg_name=?
-                             AND EXISTS (SELECT id FROM Builtins
-                             WHERE Packages.id = Builtins.id)''', (pkg,))
-        print(self._cur.fetchone())
-        return self._cur.fetchone()
+        self._cur.execute('''SELECT is_builtin FROM Packages
+                             WHERE pkg_name=?''', (pkg,))
+        try:
+            if self._cur.fetchone()[0]:
+                return True
+            else:
+                return False
+        except TypeError:
+            print(f'Package {pkg} not found in cache.')
+
+    def get_builtins(self):
+        builtins = []
+        for pkg in self.get_packages():
+            if pkg['is_builtin']:
+                builtins.append(pkg['pkg_name'])
+        return builtins
+
+    def clean_db(self):
+        pkgs = self.get_packages()
+        for pkg in pkgs:
+            if not pkg['spacemacs'] and not pkg['doomemacs']:
+                self.drop_package(pkg['pkg_name'])
+
+    def drop_suites(self):
+        for suit in ['Spacemacs', 'DoomEmacs']:
+            command = f'DELETE FROM {suit}'
+            self._cur.execute(command)
